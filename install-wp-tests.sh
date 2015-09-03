6db49374746a5939a5fd2d7cd21ae71d5a0a207e
Weston Ruter <weston@xwp.co>
2015-09-03 15:20:38 -0700

    Clean up install-wp-tests.sh

--- install-wp-tests.sh
+++ install-wp-tests.sh
@@ -14,7 +14,7 @@ WP_VERSION=${5-latest}
 WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress}
 WP_TESTS_DIR=${WP_TESTS_DIR-${WP_CORE_DIR}/tests/phpunit}
 
-set -ex
+set -e
 
 download() {
 	if [ `which curl` ]; then
@@ -26,22 +26,24 @@ download() {
 
 install_wp() {
 
-	if [ -d $WP_CORE_DIR ]; then
+	if [ -d "$WP_CORE_DIR" ]; then
 		return;
 	fi
 
-	if [ $WP_VERSION == 'latest' ]; then
+	if [ "$WP_VERSION" == 'latest' ]; then
 		local TAG=$( svn ls https://develop.svn.wordpress.org/tags | tail -n 1 | sed 's:/$::' )
-		local SVN_URL=https://develop.svn.wordpress.org/tags/$TAG/
-	elif [ $WP_VERSION == 'trunk' ]; then
+		local SVN_URL="https://develop.svn.wordpress.org/tags/$TAG/"
+	elif [ "$WP_VERSION" == 'trunk' ]; then
 		local SVN_URL=https://develop.svn.wordpress.org/trunk/
 	else
-		local SVN_URL=https://develop.svn.wordpress.org/tags/$WP_VERSION/
+		local SVN_URL="https://develop.svn.wordpress.org/tags/$WP_VERSION/"
 	fi
 
-	svn export -q $SVN_URL $WP_CORE_DIR
+	echo "Installing WP from $SVN_URL to $WP_CORE_DIR"
 
-	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/src/wp-content/db.php
+	svn export -q "$SVN_URL" "$WP_CORE_DIR"
+
+	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php "$WP_CORE_DIR/src/wp-content/db.php"
 }
 
 install_test_suite() {
@@ -52,7 +54,7 @@ install_test_suite() {
 		local ioption='-i'
 	fi
 
-	cd $WP_CORE_DIR
+	cd "$WP_CORE_DIR"
 
 	if [ ! -f wp-tests-config.php ]; then
 		cp wp-tests-config-sample.php wp-tests-config.php
@@ -71,18 +73,23 @@ install_db() {
 	local DB_SOCK_OR_PORT=${PARTS[1]};
 	local EXTRA=""
 
-	if ! [ -z $DB_HOSTNAME ] ; then
-		if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
+	if ! [ -z "$DB_HOSTNAME" ] ; then
+		if [ $(echo "$DB_SOCK_OR_PORT" | grep -e '^[0-9]\{1,\}$') ]; then
 			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
+		elif ! [ -z "$DB_SOCK_OR_PORT" ] ; then
 			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z $DB_HOSTNAME ] ; then
+		elif ! [ -z "$DB_HOSTNAME" ] ; then
 			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
 		fi
 	fi
 
+	# drop the database if it exists
+	mysqladmin drop -f "$DB_NAME" --silent --no-beep --user="$DB_USER" --password="$DB_PASS"$EXTRA || echo "$DB_NAME does not exist yet"
+
 	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS"$EXTRA
+
+	echo "DB $DB_NAME created"
 }
 
 install_wp
